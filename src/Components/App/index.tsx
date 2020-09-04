412cee232f8960c2f15e29b3fff510a743198d9e
Yao Bao <by@meetlost.com>
2020-09-05 00:14:11 +0800

    Init

---
+++ src/Components/App/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * App
+ */
+
+import React from "react";
+
+function App(): JSX.Element
+{
+  return (
+    <>
+      <div className="app-box">
+        MLChat
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * index
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import "./style.scss";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
